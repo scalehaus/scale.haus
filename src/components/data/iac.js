6f808a4712c3d49a42c66c155d9717b6d73ad5ae
sparlor <sparlor91@gmail.com>
2020-06-24 21:13:23 -0400

    ScaleHaus initial site commit

---
+++ src/components/data/iac.js
@@ -0,0 +1,14 @@
+import terraform from '../../images/terraform.svg'
+import docker from '../../images/Moby-logo.svg'
+import automate from '../../images/automate.svg'
+
+const devopsData = [
+
+    {img: terraform, text: "ScaleHaus engineers bring a wealth of terraform experience to you. We will get your environment infrastructure written, organized, and deployed.", title: "Hashicorp Terraform"},
+
+    {img: docker, text: "If your organization has yet to join the container revolution, we are here to help. We will help your team understand immutable application containers with Docker. Never be surprised in production", title: "Docker"},
+
+    {img: automate, text: "Our IaC implementation comes with automated pipelines for your infrastructure. Having infra issues in Dev and spending countless hours resolving it? Blow it away and rebuilt it with a button click.", title: "Automated Pipelines"}
+]
+
+export default devopsData;
